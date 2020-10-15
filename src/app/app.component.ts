ed31900c741e75399934d3bfc11299ae5a32fc8f
Dean Alam <s1gamingc@gmail.com>
2020-10-15 19:14:47 -0400

    added Procfile please work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'discount-calculator';
+  title = "Dean's Discount Calculator";
+  finalPrice;
+  result;
+
+  // tslint:disable-next-line: typedef
+  calculate(price: number, discount: number){
+    if(!price || !discount){
+      this.result = "Error: Please fill out both fields"
+    }
+    else{
+    let savings = (price * (discount / 100));
+    this.finalPrice = price - savings;
+    if(Number.isNaN(this.finalPrice) || Number.isNaN(savings)){
+      this.result = "Error: Please make sure you are inputting numbers";
+    }
+    else{
+    this.result = "Your final price is: $" + this.finalPrice +
+    "\n, you are saving $" + savings + "!";
+    }
+    }
+  }
 }
